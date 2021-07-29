455e21b455164d429af21c888d5c7a58cb13fd8c
TAAKICA1 <candas.kilic@swisscom.com>
2021-07-29 16:28:12 +0200

    -Lists are added. -Inputs are added -And a little nit of styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListViewComponent } from './components/list-view/list-view.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { ListViewComponent } from './components/list-view/list-view.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/list-view/list-view.component.ts
+++ src/app/components/list-view/list-view.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-list-view',
   templateUrl: './list-view.component.html',
   styleUrls: ['./list-view.component.scss']
 })
 export class ListViewComponent implements OnInit {
+  wishListOfDashi = ['first','second','third']
+  wishListOfDjuli = ['first','second','third']
+  
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }

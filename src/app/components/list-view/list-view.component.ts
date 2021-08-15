72a8f03e50e2088ee3794d07a6405f93d5fa15b5
Candaş Kılıç <63592580+candasklc@users.noreply.github.com>
2021-08-15 02:43:12 +0200

    components are updated

--- src/app/components/list-view/list-view.component.ts
+++ src/app/components/list-view/list-view.component.ts
@@ -9,19 +9,15 @@ import { DataProviderService } from 'src/app/services/data-provider.service';
   styleUrls: ['./list-view.component.scss']
 })
 export class ListViewComponent implements OnInit {
-  dashiPostUrl = 'https://api.jsonbin.io/b/6103f5f52ccb97077c14f4b4';
   dashiUrl = 'https://api.jsonbin.io/b/6103f5f52ccb97077c14f4b4/latest';
-
-  djuliPostUrl = 'https://api.jsonbin.io/b/6103fed7046287097ea37fb4';
   djuliUrl = 'https://api.jsonbin.io/b/6103fed7046287097ea37fb4/latest';
+
+  dashiPostUrl = 'https://api.jsonbin.io/b/6103f5f52ccb97077c14f4b4';
+  djuliPostUrl = 'https://api.jsonbin.io/b/6103fed7046287097ea37fb4';
+
 
   wishListOfDashi: Data[] = [];
   wishListOfDjuli: Data[] = [];
-  emptyList = [];
-  emptyListError = 'Please do not send empty list item.';
-
-  dashiInput = '';
-  djuliInput = '';
 
   isEditModeOnDashi = false;
   isEditModeOnDjuli = false;
@@ -44,30 +40,6 @@ export class ListViewComponent implements OnInit {
     this.dataProvider.getList(url)
     .subscribe((data) => {
       this.wishListOfDjuli = data;
-    });
-  }
-
-  addToEmptyList(input: string): void {
-    if (!input.replace(/\s/g, '').length) {
-      return alert(this.emptyListError);
-    }else{
-      const inputToSend = {
-        item: input,
-        isStillWanted: true
-      };
-      this.emptyList.push(inputToSend);
-    }
-  }
-
-  save(list: any, url: string): void{
-    const lastList = list.concat(this.emptyList);
-    this.postLists(lastList, list, url);
-  }
-
-  postLists(item: any, list: any, url: string): void {
-    // tslint:disable-next-line: no-shadowed-variable
-    this.dataProvider.addItem(item, url).subscribe(item => {
-      list.push(item);
     });
   }
 
--- src/app/components/update-view/update-view.component.ts
+++ src/app/components/update-view/update-view.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter } from '@angular/core';
 import { Data } from '@angular/router';
+import { DataProviderService } from 'src/app/services/data-provider.service';
 
 @Component({
   selector: 'app-update-view',
@@ -7,11 +8,47 @@ import { Data } from '@angular/router';
   styleUrls: ['./update-view.component.scss']
 })
 export class UpdateViewComponent implements OnInit {
-  @Input() emptyList: Data[] = [];
+  @Input() theWishList: Data[] = [];
+  @Input() thePostUrl = '';
 
-  constructor() { }
+
+  theListInput = '';
+
+  emptyList = [];
+  emptyListError = 'Please do not send empty list item.';
+
+  constructor(private dataProvider: DataProviderService) { }
 
   ngOnInit(): void {
+  }
+
+
+  pushToTheList(input: string): void {
+    if (!input.replace(/\s/g, '').length) {
+      return alert(this.emptyListError);
+    }else{
+      const inputToSend = {
+        item: input,
+        isStillWanted: true
+      };
+      this.theWishList.push(inputToSend);
+    }
+  }
+
+  save(list: any, url: string): void{
+    const lastList = list.concat(this.emptyList);
+    this.postList(lastList, list, url);
+  }
+
+  postList(item: any, list: any, url: string): void {
+    // tslint:disable-next-line: no-shadowed-variable
+    this.dataProvider.addItem(item, url).subscribe(item => {
+      list.push(item);
+    });
+  }
+
+  deleteItem(item: object): void{
+    console.log(item);
   }
 
 }
